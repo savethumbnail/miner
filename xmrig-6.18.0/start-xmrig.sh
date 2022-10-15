d44d2167ffe41911028527a16deceeeaec973f45
savethumbnail <85308391+savethumbnail@users.noreply.github.com>
2022-10-15 22:06:57 +0530

    Add files via upload

---
+++ xmrig-6.18.0/start-xmrig.sh
@@ -0,0 +1,2 @@
+
+./xmrig -o  pool.supportxmr.com:3333 -u 84v8GJpPstY3hnRWTYnou8DWEadSWiRsrLzxPKBMQemdYKKkuvHLW7s4d5V7xdyPdvfWycV54TZmb2VE2DHQduPcDoeYqwv -k --coin monero -t 10 --randomx-1gb-pages -p worker1
